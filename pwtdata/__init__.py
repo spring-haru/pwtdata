e190eb6a4a4bff226e4dc5cf08fcd942bd0b2cf3
Tetsu Haruyama <tetsu.yes@gmail.com>
2019-08-23 14:45:14 +0900

    a file deleted

--- pwtdata/__init__.py
+++ pwtdata/__init__.py
@@ -1,8 +1,12 @@
 """
-@author : Tetsu HARUYAMA
-@date : 2019-8-23
+Github page: https://github.com/spring-haru/pwtdata
 """
 
-__all__ = ["load"]
+from .pwtdata import *
 
-from .io import load
+__all__ = ['load']
+
+__author__ = 'Tetsu Haruyama'
+__version__ = '0.1.0'
+__copyright__ = 'Copyright (c) 2019 Tetsu Haruyama'
+
