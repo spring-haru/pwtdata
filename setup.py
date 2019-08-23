af17288fa6f7ebdb73340c56b1458dccf2ce7a27
Tetsu Haruyama <tetsu.yes@gmail.com>
2019-08-23 15:55:56 +0900

    initial upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='pwtdata',
+      packages=find_packages(),
+      package_dir={'pwtdata': './pwtdata'},
+      version='0.1.0',
+      description='Python package for downloading the Penn World Tables dataset.',
+      author='Tetsu HARUYAMA',
+      author_email='haruyama@econ.kobe-u.ac.jp',
+      url='https://github.com/spring-haru/pwtdata',
+      license='LICENSE.rst',
+      install_requires=['pandas'],
+      classifiers=['Intended Audience :: Education',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   ]
+      )
