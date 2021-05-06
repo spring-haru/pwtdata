65799993eb2cc7a2f6bb5100c983b0e036f18a65
Tetsu Haruyama <tetsu.yes@gmail.com>
2021-05-06 20:02:28 +0900

    additional files in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup, find_packages
+import glob
+import os
+import sys
+from setuptools import find_packages, setup
+
+
+additional_files = []
+for filename in glob.iglob('./pwtdata/**', recursive=True):
+    if '.csv.bz' in filename:
+        additional_files.append(filename.replace('./pwtdata/', ''))
+
 
 setup(name='pwtdata',
       packages=find_packages(),
       package_dir={'pwtdata': './pwtdata'},
       version='0.2.0',
-      description='Python package for downloading the Penn World Tables dataset.',
+      description='Python package containing the Penn World Tables dataset.',
       author='Tetsu HARUYAMA',
       author_email='haruyama@econ.kobe-u.ac.jp',
       url='https://github.com/spring-haru/pwtdata',
